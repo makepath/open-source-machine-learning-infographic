f7d2d87d283c0567dc52376d658c8e7fb32319ba
Eduardo Mazzucco Cividini <edumazzucco@hotmail.com>
2022-08-05 14:18:14 -0300

    feat: zoomable-circle-packing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import GlobalStyles from "./styles/global.styles";
 
@@ -9,18 +7,7 @@ function App() {
       <GlobalStyles />
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>Oi</p>
         </header>
       </div>
     </>
