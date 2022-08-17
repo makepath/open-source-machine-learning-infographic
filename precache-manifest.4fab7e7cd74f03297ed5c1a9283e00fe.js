80129f2c780c9899b876d839c5d5f942cc78be3a
Eduardo Mazzucco Cividini <edumazzucco@hotmail.com>
2022-08-17 15:29:10 -0300

    Updates

---
+++ precache-manifest.4fab7e7cd74f03297ed5c1a9283e00fe.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "65c0c80288e439829021",
+    "url": "/open-source-machine-learning-infographic/static/css/main.81cd7c18.chunk.css"
+  },
+  {
+    "revision": "65c0c80288e439829021",
+    "url": "/open-source-machine-learning-infographic/static/js/main.65c0c802.chunk.js"
+  },
+  {
+    "revision": "5e8bda3153763d6a3143",
+    "url": "/open-source-machine-learning-infographic/static/js/runtime~main.5e8bda31.js"
+  },
+  {
+    "revision": "3232b42cbf6bc2b873e2",
+    "url": "/open-source-machine-learning-infographic/static/js/2.3232b42c.chunk.js"
+  },
+  {
+    "revision": "ebf1b7c3ce667302c81e3907e36bdaaf",
+    "url": "/open-source-machine-learning-infographic/static/media/Right-Snake-Tail.ebf1b7c3.svg"
+  },
+  {
+    "revision": "8edbbf9ae3b6a0a0457a65bbddeab63d",
+    "url": "/open-source-machine-learning-infographic/static/media/Loop-Snake.8edbbf9a.svg"
+  },
+  {
+    "revision": "60ed3bb6c0526a958e0f90ea071485c8",
+    "url": "/open-source-machine-learning-infographic/static/media/Right-Snake.60ed3bb6.svg"
+  },
+  {
+    "revision": "09cdd625f2dfd3ba32fd86417d840ee1",
+    "url": "/open-source-machine-learning-infographic/static/media/Left-Snake-Head.09cdd625.svg"
+  },
+  {
+    "revision": "4154564589cbc5ac5a7e71a7f7321e2c",
+    "url": "/open-source-machine-learning-infographic/static/media/Right-Snake-Head.41545645.svg"
+  },
+  {
+    "revision": "b01fa118a340b180f68f1fb708b6cd4b",
+    "url": "/open-source-machine-learning-infographic/static/media/Right-Head.b01fa118.svg"
+  },
+  {
+    "revision": "f55e6e6014f6e1692edbb46a156d65b6",
+    "url": "/open-source-machine-learning-infographic/static/media/Acumin-BdPro.f55e6e60.otf"
+  },
+  {
+    "revision": "d3085f686df272f9e1a267cc69b2d24f",
+    "url": "/open-source-machine-learning-infographic/static/media/Montserrat-Bold.d3085f68.ttf"
+  },
+  {
+    "revision": "bdb37477b68d098317daaf939f295527",
+    "url": "/open-source-machine-learning-infographic/static/media/Acumin-RPro.bdb37477.otf"
+  },
+  {
+    "revision": "2a3a23bf878d7187dc1e20049a44e9ba",
+    "url": "/open-source-machine-learning-infographic/static/js/2.3232b42c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7903754b9e5b4161e66c0b580b84900b",
+    "url": "/open-source-machine-learning-infographic/index.html"
+  }
+];
